c493c970a037df039642fe82daa332cea28670ca
Emily Dias <emilydias.ec@gmail.com>
2024-12-30 15:08:44 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,34 @@
+# Tipos e prints:
+numero = 42         # int
+altura = 1.75       # float
+nome = "Emily"     # str
+ativo = True        # bool
+lista = [1, 2, 3]   # list
+
+# Saídas
+print(f"Meu número favorito é {numero}.")  
+print(f"Minha altura é {altura} metros.")  
+print(f"Eu amo aprender {nome}!")  
+print(f"Está ativo? {ativo}")  
+print(f"Minha lista: {lista}")#
+
+
+print("nome: Emi")
+
+print("Nome: ")
+
+valor = 8 #valor é a variavel e 10 é a memoria que ele armazena
+print("Valor = ", valor)
+
+nome = "Camila" #antes do texto (string) sempre colocamos parenteses
+
+#Forma raiz
+#print("Nome:", nome, "\nValor:",valor) #/n essa barra e o n antes da informação faz o conteudo descer para linha debaixo, dentro do mesmo print
+
+#Print F (forma mais usada)
+print(f"Meu nome e {nome} e eu tenho {valor} anos.")
+
+#parei no video 5 tipos de formatações print
+
+
+numero1 = int(input("Digite o primeiro número: "))
